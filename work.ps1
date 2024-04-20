c117cecafebc97166dad8c7bd3bb2604df4f5d88
Alex Montoya <NeuroPyrox@Gmail.com>
2024-04-20 05:21:38 -0600

    Only set $foundRooms if unset

--- work.ps1
+++ work.ps1
@@ -551,7 +551,9 @@ Function Process-Room-With-Retry {
     }
 }
 
-$foundRooms = [System.Collections.ArrayList]@();
+if ($foundRooms -eq $null) {
+    $foundRooms = [System.Collections.ArrayList]@();
+}
 Function Main {
     Param([int]$startRoom);
     $roomNumbers = $(101..103; 105; 126..129; 201..214; 216..229; 231; 301..329; 331; 401..429; 431);
